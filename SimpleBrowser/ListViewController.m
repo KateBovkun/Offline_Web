affc2ae5dd0bff12458027772065bd5d43c66299
KateBovkun <kbovkun@gmail.com>
2012-03-22 21:12:58 +0700

    Add images for test cache

--- SimpleBrowser/ListViewController.m
+++ SimpleBrowser/ListViewController.m
@@ -41,8 +41,9 @@
     NSLog(@"%@", urlString);
     if ([cache isCached:[NSURL URLWithString:urlString]]) 
     {
-        NSData * urlData = [cache getCachedFile:[NSURL URLWithString:urlString]];
-        [listWebView loadData:urlData MIMEType:@"text/html" textEncodingName:@"UTF8" baseURL:nil];
+        //NSData * urlData = [cache getCachedFile:[NSURL URLWithString:urlString]];
+        [listWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString] cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:100]];
+        //[listWebView loadData:urlData MIMEType:@"text/html" textEncodingName:@"UTF8" baseURL:nil];
     }
 
 }
@@ -70,7 +71,7 @@
 {
     if (section == 0) 
     {
-        return 21;
+        return 41;
     }
     return 0;
 }
@@ -114,6 +115,26 @@
         @"http://ddarchive.net/wp-projects/ipad2/html5-test/articles/article-18.html",
         @"http://ddarchive.net/wp-projects/ipad2/html5-test/articles/article-19.html",
         @"http://ddarchive.net/wp-projects/ipad2/html5-test/articles/article-20.html",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/06/Others/Images/2012-03-06/v%201_1331040840.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/06/Production/Digital/homepage/Images/509058895.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/06/Local/Images/MONITOR006_1331054520.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/08/National-Security/Images/Mideast_Syria_0baa3.jpg",
+        @"http://www.washingtonpost.com/rf/image_404h/2010-2019/WashingtonPost/2012/03/05/Production/Daily/A-Section/Images/139060869.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/07/Business/Images/140879311.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/07/Sports/Images/135802009.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/08/Interactivity/Images/3_Harris_McCain.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/WashingtonPost/Content/Production/Blogs/2chambers/Images/Congress_Republicans_08309.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/06/Interactivity/Images/140720904.jpg",
+        @"http://www.washingtonpost.com/rf/image_404h/2010-2019/WashingtonPost/2012/03/04/Others/Images/2012-03-04/HOMETECH001_1330880161.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/08/Sports/Images/140941542.jpg",
+        @"http://www.washingtonpost.com/rf/image_404h/WashingtonPost/Content/Blogs/early-lead/Images/Missouri_Kansas_Basketball_0bb32.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/07/Local/Images/Jobs03_1331159761.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/05/Interactivity/Images/iStock_000010084652Small.jpg",
+        @"http://www.washingtonpost.com/rf/image_404h/2010-2019/WashingtonPost/2012/03/01/Health-Environment-Science/Images/63976fdc-63d6-11e1-b51c-01d913513d63_UUIDsciencenews6.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/07/Others/Images/2012-03-06/y%204a_1331083441.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/2010-2019/WashingtonPost/2012/03/07/Style/Images/kucinich--452x273.jpg?uuid=-7zdCmilEeGuP_AdkGeSuQ",
+        @"http://img.wpdigital.net/rf/image_606w/2010-2019/WashingtonPost/2012/03/06/Food/Videos/03062012-67v/03062012-67v.jpg",
+        @"http://www.washingtonpost.com/rf/image_606w/WashingtonPost/Content/Blogs/arts-post/Images/509485442.jpg",
      nil] retain];
 }
 
